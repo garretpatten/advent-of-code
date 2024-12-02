a10cbd636c6e1ec60da69e740185f872ee154c35
Garret Patten <garret.patten@proton.me>
2024-12-02 16:45:12 -0500

    aoc2024: day 1 solution

--- 2024/day1/day1.js
+++ 2024/day1/day1.js
@@ -1,1 +1,31 @@
-// TODO
+// Node Dependencies
+const fs = require('fs');
+
+function solve() {
+    const leftList = [];
+    const rightList = [];
+
+    const inputMetadata = fs.readFileSync('./input.txt', 'utf8');
+    let items;
+    let delimiter = '   ';
+    for (let line of inputMetadata.split('\n')) {
+        if (line.indexOf(delimiter) !== -1) {
+            items = line.split('   ');
+            leftList.push(Number(items[0]));
+            rightList.push(Number(items[1]));
+        }
+    }
+
+    const sortedLeftList = leftList.sort();
+    const sortedRightList = rightList.sort();
+
+    let acc = 0;
+    for (let i = 0; i < leftList.length; i++) {
+        acc += Math.abs(sortedLeftList[i] - sortedRightList[i]);
+    }
+
+    console.log('The solution is: ' + acc);
+    return acc;
+}
+
+solve();
